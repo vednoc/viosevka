3d9289704af351bfe48cfeff589541d77f3e4eda
vednoc <vednoc@protonmail.com>
2021-04-15 07:04:58 +0200

    build: add function that checks if commands exist

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ URL="https://github.com/be5invis/Iosevka/archive/refs/tags/v$VERSION.tar.gz"
 DIR="Iosevka-$VERSION"
 
 # Color helpers.
+RED="\033[0;31m"
 GRN="\033[0;32m"
 CLR="\033[0m"
 
@@ -13,6 +14,16 @@ log() {
     printf "$GRN%s$CLR %s\n" "$(date +%F\ %H:%M:%S)" "$@"
 }
 
+lor() {
+    printf "$RED%s$CLR %s\n" "$(date +%F\ %H:%M:%S)" "$@"
+}
+
+cmd() {
+    if ! command -v "$1" >/dev/null; then
+        lor "Missing '$1' command. Build stopped."
+        exit 1
+    fi
+}
 
 download() {
     if [ -d "$DIR" ]; then
@@ -25,6 +36,7 @@ download() {
 
 build() {
     cd $DIR
+    cmd npm
 
     log "Installing NPM dependencies."
     npm install
