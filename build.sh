8676a134b431fb14e35aabc199f18bcad0c777c5
vednoc <vednoc@protonmail.com>
2021-04-15 06:47:26 +0200

    build: add font helpers and download functionality

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env sh
 
+# Font helpers.
+VERSION="6.0.0-preview.1"
+URL="https://github.com/be5invis/Iosevka/archive/refs/tags/v$VERSION.tar.gz"
+DIR="Iosevka-$VERSION"
+
 # Color helpers.
 GRN="\033[0;32m"
 CLR="\033[0m"
@@ -8,8 +13,18 @@ log() {
     printf "$GRN%s$CLR %s\n" "$(date +%F\ %H:%M:%S)" "$@"
 }
 
+
+download() {
+    if [ -d "$DIR" ]; then
+        log "Source code is already available."
+    else
+        log "Downloading and extracting source code."
+        curl -L $URL | tar xvz
+    fi
+}
+
 main() {
-    log "Hello, World!"
+    download
 }
 
 main
